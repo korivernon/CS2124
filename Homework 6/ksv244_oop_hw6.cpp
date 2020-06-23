17332e814347db11ab068402f525c3312986c764
Kori Vernon <korivernon@nyu.edu>
2020-06-23 13:45:35 -0400

    latest

--- Homework 6/ksv244_oop_hw6.cpp
+++ Homework 6/ksv244_oop_hw6.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 class StringSet {
     private:
-        // double weight; // kg -> 90.9
-        // int dob; // 07232000
-        // string name; // kori
         string *set;
         size_t size,cap;
         void resize(const size_t& newsize); // helper function to resize the array
@@ -29,13 +26,46 @@ class StringSet {
         StringSet operator*(const StringSet& self);
         string& operator[](const int i) {return set[i];} //return by value
         string operator[](const int i) const {return set[i];} //return by reference
+        bool operator==(const StringSet& self); //equivalence operator
         //clear function
         void clear();
         //return the size of the string set
         int sizeset()const {return (int)size;}
         friend ostream& operator<<(ostream& outs, const StringSet& ss);
+        // getters 
+        int getCap() const {return (int)cap;}
+        int find(const string& elem) const; // find function just because..
 };
 
+int StringSet::find(const string& elem) const{
+    for (size_t i = 0; i < size; i++){
+        if (elem == set[i])
+            return i;
+    }
+    return -1; // if not found 
+}
+
+bool StringSet::inSet(const string& str) const{ // determine whether or not an element is inside of the set
+    for(size_t i = 0; i < size; i++ ){
+        if(set[i] == str)
+            return true;
+    }
+    return false;
+}
+StringSet::StringSet(string arr []) : cap(5), size(0), set(new string[5]) { // initialize set to 5, cap to 5, and size to 0
+    int i = 0;
+    while (arr[i] != "-1"){ // enter in a string array where the last element will be "-1". 
+        if (i == cap){ // continue to iterate through the array until we reach the -1, which is the last element
+            resize(2*cap); // resize if necessary
+        }
+        set[i] = arr[i];
+        size++;
+        i++;
+    }
+}
+
+
+// copy operator
 StringSet& StringSet::operator=(const StringSet& self){
     if(this == &self)// if self assignment
         return *this;
@@ -48,24 +78,7 @@ StringSet& StringSet::operator=(const StringSet& self){
         set[i] = self.set[i];
     return *this; // return *this
 }
-bool StringSet::inSet(const string& str) const{
-    for(size_t i = 0; i < size; i++ ){
-        if(set[i] == str)
-            return true;
-    }
-    return false;
-}
-StringSet::StringSet(string arr []) : cap(5), size(0), set(new string[5]) {
-    int i = 0;
-    while (arr[i] != "-1"){
-        if (i == cap){
-            resize(2*cap);
-        }
-        set[i] = arr[i];
-        size++;
-        i++;
-    }
-}
+//overloaded operators
 StringSet StringSet::operator*(const StringSet& self){
     StringSet temp;
     temp.resize(size+self.size);//go ahead and make the capacity both
@@ -77,26 +90,35 @@ StringSet StringSet::operator*(const StringSet& self){
     }
     return temp;
 }
-StringSet StringSet::operator+(const StringSet& self){
+StringSet StringSet::operator+(const StringSet& self){ // addition operator for stringset
     StringSet temp;
     temp.resize(size + self.size);
-    for(size_t i = 0; i < size; i++)
+    for(size_t i = 0; i < size; i++) // iterate through both and add them both to temp
         temp.add(set[i]);
     for(size_t i = 0; i < self.size; i++)
         temp.add(self.set[i]);
     return temp;
 }
-
-ostream& operator<<(ostream& outs, const StringSet& ss){
+// extra
+bool StringSet::operator==(const StringSet& self){
+    for (size_t i = 0; i < size; i++){
+        if (!self.inSet(set[i]))
+            return false;
+    }
+    return true;
+}
+// ostream operator
+ostream& operator<<(ostream& outs, const StringSet& ss){ // prefer this for output
     outs << "{";
     for (size_t i = 0; i < ss.size; i++){
-        if (i != ss.size-1) {cout << ss.set[i] << ",";}
-        else {cout << ss.set[i];}
+        if (i != ss.size-1) {outs << ss.set[i] << ",";}
+        else {outs << ss.set[i];}
     }
     outs << "}";
     return outs;
 }
-void StringSet::resize(const size_t& newsize){
+// resize function
+void StringSet::resize(const size_t& newsize){ //resize function to change the size if size reaches cap
     string * temp = new string[newsize];
     if(newsize > size){
         for(size_t i = 0; i < size; i++)
@@ -110,13 +132,11 @@ void StringSet::resize(const size_t& newsize){
     delete [] set;
     set = temp;
 }
+
 void StringSet::add(const string& add){
     if (size == cap)
-        resize(cap*2);
-    if (!inSet(add))
-        set[size++] = add;
-    else
-        cout << "String already in set. " << endl;
+        resize(cap*2); // if cap is the same as size then resize 
+    set[size++] = add; // add the value and then increment 
 }
 void StringSet::remove(const string& rm){
     if (!inSet(rm)) //assuing there will only be one string that we are removing and not multiple --  the first instance of the string will be removed
@@ -142,10 +162,15 @@ void StringSet::remove(const string& rm){
         }
     }
 }
+// clear function
 void StringSet::clear() {
     delete [] set;
-    size = cap = 0; //assuming that nothing will be readded
+    string * temp = new string[5]; //reset cap
+    set = temp; // make set temp
+    size = 0;
+    cap = 5; //assuming that nothing will be readded
 }
+
 int main(){
     std::string set[5];
     set[0] = "give";
@@ -178,11 +203,13 @@ int main(){
     set2[0] = "give";
     set2[1] = "I'll";
     set2[2] = "think";
-    set2[3] = "about";
-    set2[4] = "pun";
+    set2[3] = "for";
+    set2[4] = "this";
     set2[5] = "-1";
     cout << "hi" << endl;
     StringSet xinyu(set2);
+
+    //intersection set == {give, for this}
     
 
     cout << "xinyu: " << xinyu << endl;
@@ -197,13 +224,16 @@ int main(){
     katz = kori * xinyu;
 
     std::cout << "Intersection: " << katz << std::endl;
+    // this produces the intersection, but for some reason it does not output it.. and I dont know
     cout << katz.sizeset() << endl;
     cout << katz << endl;
     katz.add("kori"); // this is being added but the element before isn't
+
     cout << katz[1] << endl;
-    //kori.clear();
-    //std::cout << "Cleared Function: " << kori << std::endl;
-    //delete [] set;
-    //delete [] set2;
+    cout << "Capacity: " << katz.getCap() << endl;
+    katz.add("new");
+    cout << "Capacity: " << katz.getCap() << endl;
+    kori.clear();
+    std::cout << "Cleared Function: " << kori << std::endl;
 }
 
